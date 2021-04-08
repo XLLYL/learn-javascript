fd75e0f7437cb7865ad7c0202aca86c8993d1ec5
xiaolei <xiaolei@linklogis.com>
2021-04-09 00:25:36 +0800

    权威指南概览部分

---
+++ 1_js概览.js
@@ -0,0 +1,19 @@
+/**
+ * 1."短语是表达式的话,整个句子就称作语句"
+ * 表达式计算出一个值(arr[0])，语句则改变程序的运行状态,例如有:变量声明语句,赋值语句,控制结构语句(条件判断和循环)
+ * 
+ * 2.当函数赋值给对象的属性,我们称之为"方法"
+ * 
+ * 3.Js是一种面向对象的编程语言
+ * */
+ 
+ /**
+  * 事件处理程序 event handler
+  * <button onclick="a(this)">按钮</button> -> onclick 是 button元素的方法，this 是 button 元素
+  * 
+  * <button onclick="a()">按钮</button>
+  * <script>function a(){console.log(this)}</script> -> a() 是函数调用,this 指向全局对象 Window
+  * */
+  
+  
+ 
